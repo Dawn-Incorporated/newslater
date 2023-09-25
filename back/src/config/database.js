7135014cceda24f2c6eafa8ae86c355d5cec9b3a
matisbyar <matis.byar.06@icloud.com>
2023-09-25 15:24:38 +0200

    newslater. 0.0.3-alpha Release notes: - Updated Connection to database so that it doesn't crashes the app

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -1,22 +1,34 @@
 const {log} = require("byarutils/lib/logger");
 
-const connection = require('mysql2').createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-})
+const connection = () => {
+    require('mysql2').createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME,
+    })
+}
 
 async function execute(query, params) {
-    return new Promise((resolve, reject) => {
-        connection.query(query, params, (error, results, fields) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(results);
-            }
+    try {
+        return new Promise((resolve, reject) => {
+            connection.query(query, params, (error, results, fields) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
         });
-    });
+    } catch (error) {
+        log("ERROR", "Database", "Failed to execute query " + query + " with params " + params + ". " + error);
+    }
+}
+
+try {
+    connection.connect();
+} catch (error) {
+    log("ERROR", "Database", "Failed to connect to database. " + error);
 }
 
 
