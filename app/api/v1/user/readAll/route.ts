7cd625ec03d9160fbc3ee626e3c1623a5568e101
vanhovev <vanhove.valentin@gmail.com>
2024-04-26 11:26:40 +0200

    Add return with all route

--- app/api/v1/user/readAll/route.ts
+++ app/api/v1/user/readAll/route.ts
@@ -1,6 +1,5 @@
 const user = require('@api/controller/UserController');
 
-
 export async function GET() {
     const users = await user.getUsersApi();
     return new Response(JSON.stringify(users), {status: 200});
