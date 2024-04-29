d9307f5244053fee10d32b29c1bd34a5b7f48323
vanhovev <vanhove.valentin@gmail.com>
2024-04-29 11:14:39 +0200

    New page : View feed

--- app/api/controller/FeedController.js
+++ app/api/controller/FeedController.js
@@ -3,16 +3,19 @@
 const FeedRepository = require('@api/model/FeedRepository');
 const feed = new FeedRepository();
 
+function create(req, res) {
+    feed.create(req, res);
+}
+function getAll() {
+    return feed.getAll();
+}
+
 function getByNameApi(req, res) {
     feed.getByNameApi(req, res);
 }
 
-function create(req, res) {
-    feed.create(req, res);
-}
-
-
 module.exports = {
     create,
+    getAll,
     getByNameApi
 }
--- app/api/services/rss-retriever.js
+++ app/api/services/rss-retriever.js
@@ -52,5 +52,6 @@ function retrieveFeeds(links) {
 }
 
 module.exports = {
+    retrieveFeed,
     retrieveFeeds
 }
