798785d62acbfd78f670abcdeb92e36fbeb412b6
Matis Byar <matis.byar.06@icloud.com>
2024-05-03 18:22:00 +0200

    - Cleaned homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-import {Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList} from "@/components/ui/breadcrumb";
-
 export default function Home() {
     return (
         <main>
-            <div className="w-full m-4 px-4">
-                <Breadcrumb>
-                    <BreadcrumbList>
-                        <BreadcrumbItem>
-                            <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                        </BreadcrumbItem>
-                    </BreadcrumbList>
-                </Breadcrumb>
-            </div>
-            <h1 className={"absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-2xl animate-pulse"}>Development in progress...</h1>
+            <h1 className={ "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-2xl animate-pulse" }>Development in progress...</h1>
         </main>
     );
 }
