8784d12273b1bddfbcbe28c9ba5dde2af2692cfa
vanhovev <vanhove.valentin@gmail.com>
2024-05-16 13:49:27 +0200

    fix: Recovery of email style

--- src/server/services/html-generator.ts
+++ src/server/services/html-generator.ts
@@ -10,7 +10,6 @@ export function html(firstname: string, feeds: any[]) {
         <div class="logo">
             <img src="https://i.ibb.co/c690ymG/newslater.png" alt="newslater." style="width: 200px;">
         </div>
-        <p>hello ${firstname}.</p>
     `;
 
     // Feeds
@@ -19,18 +18,21 @@ export function html(firstname: string, feeds: any[]) {
         html += `
             <h1>${feed[0].websiteTitle}</h1>
             ${makeFeed(feed)}
-            <hr>
         `;
     }
 
     // Style
     html += `
         <style>
+            .logo { display: flex; justify-content: center; margin-bottom: 50px;}
+            .hello {  display: flex; justify-content: center; margin-bottom: 20px;}
             body { background-color: transparent;}
-            h2 { font-size: 24px; color: #666; }
+            h2 { font-size: 24px; }
+            h1 { display: flex; justify-content: center;}
             p, .subtitle { font-size: 16px; line-height: 1.5; color: #888; }
             a { text-decoration: none; color: #0077cc; }
             img { max-width: 100%; max-height: fit-content }
+            .feed { padding: 1.5rem; margin-bottom: 20px; margin-inline: 5px; border-radius: 0.75rem; box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.3), 0 1px 2px -1px rgb(0 0 0 / 0.1); }
         </style>
     `;
 
@@ -48,15 +50,17 @@ export function html(firstname: string, feeds: any[]) {
 function makeFeed(feed: any[]) {
     let feedHtml = [];
     for (let publication of feed) {
-            feedHtml.push(`
+        feedHtml.push(`
+            <div class="feed">
             <h2>${publication.title}</h2>
             <div class="subtitle">
                 ${makeSubtitles(publication)}
             </div>
             <p>${publication.content}</p>
+            </div>
         `);
     }
-    return feedHtml.join(' --- ');
+    return feedHtml.join('');
 }
 
 /**
@@ -68,7 +72,14 @@ function makeSubtitles(publication: any) {
     let subtitles = [];
     if (publication.creator) subtitles.push(publication.creator);
     if (publication.description) subtitles.push(publication.description);
-    if (publication.pubDate) subtitles.push(new Date(publication.pubDate).toLocaleDateString());
-    if (publication.websiteLink) subtitles.push(`<a class="website" href="${publication.websiteLink}">open ${publication.websiteTitle}.</a>`);
-    return subtitles.join(' • ');
+    if (publication.pubDate) subtitles.push(new Date(publication.pubDate).toLocaleString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    }));
+    if (publication.websiteLink) subtitles.push(`<a class="website" href="${publication.link}">${publication.websiteTitle}.</a>`);
+    return subtitles.join(' - ');
 }
