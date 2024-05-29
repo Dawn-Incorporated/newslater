41cdbe0250ac2f96305624c7ec67c51e807b1b50
Valentin Vanhove <92478691+vanhovev@users.noreply.github.com>
2024-05-29 08:22:57 +0200

    improve: cron jobs

--- src/app/api/v1/cron/route.ts
+++ src/app/api/v1/cron/route.ts
@@ -40,11 +40,11 @@ async function sendMail() {
 }
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const key = searchParams.get("key");
-
-  if (key !== process.env.CRON_SECRET) {
-    return new Response("Unauthorized", { status: 401 });
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
   }
 
   const cron = await sendMail();
