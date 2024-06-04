6ca73e4626517754db9d497f2b2f84f8e196ada7
Matis Byar <matis.byar.06@icloud.com>
2024-06-04 15:20:08 +0200

    refactor: imported type instead of object

--- src/server/auth/auth.config.ts
+++ src/server/auth/auth.config.ts
@@ -1,6 +1,6 @@
 import { env } from "@/env";
 import emailForMagicLink from "@/server/auth/sendVerificiationRequestEmail";
-import { NextAuthConfig } from "next-auth";
+import type { NextAuthConfig } from "next-auth";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Resend from "next-auth/providers/resend"
