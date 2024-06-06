5ed45f4523460ed9935567565cf41a40b0f09082
Valentin Vanhove <92478691+vanhovev@users.noreply.github.com>
2024-06-06 13:42:45 +0200

    improve: change 'from' in mailer

--- src/server/config/mailer.ts
+++ src/server/config/mailer.ts
@@ -7,7 +7,7 @@ const resend = new Resend(env.AUTH_RESEND_KEY);
 
 export async function send(to: string, subject: string, html: ReactElement) {
     const send = await resend.emails.send({
-        from: 'Newslater by Dawn. <no-reply@newslater.vanhovev.com>',
+        from: 'Newslater by Dawn. <no-reply@newslater.fr>',
         to: to,
         subject: subject,
         react: html,
