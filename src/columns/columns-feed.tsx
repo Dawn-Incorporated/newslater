9bd2f5dcf1371653d65ce1f553144ee5e1099e17
Valentin Vanhove <92478691+vanhovev@users.noreply.github.com>
2024-06-13 15:30:15 +0200

    rollback: table actions

--- src/columns/columns-feed.tsx
+++ src/columns/columns-feed.tsx
@@ -1,15 +1,95 @@
-import { nameCell } from "@/columns/name-cell";
+"use client"
 
-const handleUnfollowFeed = (row : any) => {
-	console.log({...row.original, type: "unfollow"});
-}
+import { ColumnDef } from "@tanstack/react-table"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import React from "react";
+import { Checkbox } from "@/components/ui/checkbox";
+import { FeedType } from "@/server/db/types";
 
-const handleVerifyFeed = (row : any) => {
-	console.log({...row.original, type: "verify"});
-}
+export const columnsFeed: ColumnDef<FeedType>[] = [
+	{
+		id: "select",
+		header: ({table}) => (
+			<Checkbox
+				checked={
+					table.getIsAllPageRowsSelected() ||
+					(table.getIsSomePageRowsSelected() && "indeterminate")
+				}
+				onCheckedChange={ (value) => table.toggleAllPageRowsSelected(!!value) }
+				aria-label="Select all"
+			/>
+		),
+		cell: ({row}) => (
+			<Checkbox
+				checked={ row.getIsSelected() }
+				onCheckedChange={ (value) => row.toggleSelected(!!value) }
+				aria-label="Select row"
+			/>
+		),
+	},
+	{
+		accessorKey: "name",
+		header: ({column}) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={ () => column.toggleSorting(column.getIsSorted() === "asc") }
+				>
+					Name
+					<ArrowUpDown className="ml-2 h-4 w-4"/>
+				</Button>
+			)
+		}
+	},
+	{
+		accessorKey: "description",
+		header: "Description"
+	},
+	{
+		accessorKey: "url",
+		header: "URL",
+	},
+	{
+		accessorKey: "categorie",
+		header: ({column}) => {
+			return (
+				<Button
+					variant="ghost"
+					onClick={ () => column.toggleSorting(column.getIsSorted() === "asc") }
+				>
+					Category
+					<ArrowUpDown className="ml-2 h-4 w-4"/>
+				</Button>
+			)
+		}
+	},
+	{
+		id: "actions",
+		cell: ({row}) => {
+			const feed = row.original
 
-const handleDeleteFeed = (row : any) => {
-	console.log({...row.original, type: "delete"});
-}
-
-export const columnsFeed = nameCell(handleUnfollowFeed, handleVerifyFeed, handleDeleteFeed);
+			return (
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" className="h-8 w-8 p-0">
+							<span className="sr-only">Open menu</span>
+							<MoreHorizontal className="h-4 w-4"/>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuLabel>Actions</DropdownMenuLabel>
+						<DropdownMenuItem
+							onClick={ () => navigator.clipboard.writeText(feed.url) }
+						>
+							Copy RSS
+						</DropdownMenuItem>
+						<DropdownMenuSeparator/>
+						<DropdownMenuItem>Open Feed</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			)
+		}
+	}
+]
