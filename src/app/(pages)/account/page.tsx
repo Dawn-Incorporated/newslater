11882178e5b594503ded25fa4ea098b6013cd45f
Matis Byar <matis.byar.06@icloud.com>
2024-06-28 22:25:49 +0200

    impr: removed security wrappers

--- src/app/(pages)/account/page.tsx
+++ src/app/(pages)/account/page.tsx
@@ -1,12 +1,13 @@
 import { AuthenticatedAccountPage } from "@/app/(pages)/account/(authenticated)/root";
 import { auth } from "@/auth";
 import { notFound } from "next/navigation";
+import { SignIn } from "@/app/(pages)/account/(unauthenticated)/sign-in";
 
 export default async function Followed() {
     const session = await auth()
 
     if (!session?.user) {
-        return notFound();
+        return <SignIn />
     }
 
     return (
