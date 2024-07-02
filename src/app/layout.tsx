67e11f40a5594dc706a701cf7eeecc6683555216
Valentin Vanhove <92478691+vanhovev@users.noreply.github.com>
2024-07-02 19:20:37 +0200

    feat: loading page for /

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,22 +8,21 @@ import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
-    title: "Newslater",
-    description: "Send newsletters to your subscribers."
+	title: "Newslater",
+	description: "Send newsletters to your subscribers."
 };
 
-export default function RootLayout({children}: Readonly<{ children: ReactNode; }>) {
-    return (
-        <html lang="en">
-        <body className={ inter.className }>
-        <NextAuthProvider>
-            { children }
-            <Toaster/>
-        </NextAuthProvider>
-        <Toaster richColors={ true }/>
-        <Analytics />
-        <SpeedInsights />
-        </body>
-        </html>
-    );
+export default function RootLayout({children}: Readonly<{children: ReactNode}>) {
+	return (
+		<html lang="en">
+		<body className={ inter.className }>
+		<NextAuthProvider>
+			{ children }
+		</NextAuthProvider>
+		<Toaster richColors={ true }/>
+		<Analytics/>
+		<SpeedInsights/>
+		</body>
+		</html>
+	);
 }
