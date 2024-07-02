2179f27f5a53a29de95af83abc86792d0ce71963
Valentin Vanhove <92478691+vanhovev@users.noreply.github.com>
2024-07-02 20:20:34 +0200

    impr: remove unused sidebar in mobile

--- src/components/app/nav/navbar.tsx
+++ src/components/app/nav/navbar.tsx
@@ -29,25 +29,6 @@ export default async function Navbar() {
                         <h1 className={cn(libre_baskerville.className, "header")}>newslater.</h1>
                     </Link>
                 </nav>
-                <Sheet>
-                    <SheetTrigger asChild>
-                        <Button
-                            variant="outline"
-                            size="icon"
-                            className="shrink-0 md:hidden"
-                        >
-                            <Menu className="h-5 w-5"/>
-                            <span className="sr-only">Toggle navigation menu</span>
-                        </Button>
-                    </SheetTrigger>
-                    <SheetContent side="left">
-                        <nav className="grid gap-6 text-lg font-medium">
-                            <Link href="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
-                                <h1 className={cn(libre_baskerville.className, "header")}>newslater.</h1>
-                            </Link>
-                        </nav>
-                    </SheetContent>
-                </Sheet>
                 <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
                     <form className="ml-auto flex-1 sm:flex-initial">
                         <div className="relative">
