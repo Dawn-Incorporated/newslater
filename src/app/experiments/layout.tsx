4be643099d5612e7923d8ccde009e6228b2066b8
Matis Byar <matis.byar.06@icloud.com>
2025-03-10 18:16:06 +0100

    feat: add ExperimentsLayout component for organizing experiments and tests

---
+++ src/app/experiments/layout.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+export default function ExperimentsLayout({children}: { children: ReactNode }) {
+    const pathname = usePathname();
+    
+    return (
+        <main className="flex flex-col w-screen h-screen">
+            <div className="flex flex-col border-b w-screen p-4 pb-2 gap-2">
+                <div className="flex gap-2 items-baseline">
+                    <h1 className="text-xl font-bold">Experiments</h1>
+                    <p>A collection of experiments and tests.</p>
+                </div>
+                <div className="flex flex-col">
+                    <Link href="/experiments/read-rss" className={cn("rounded w-fit p-1", pathname.includes("/read-rss") ? "underline" : "")}>Read RSS</Link>
+                </div>
+            </div>
+            
+            { children }
+        </main>
+    )
+    
+}
